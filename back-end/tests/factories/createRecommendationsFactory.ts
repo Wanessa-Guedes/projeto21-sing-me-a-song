f64350a128f17c656b33d1a533993b94a4fb93fa
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-26 22:12:09 -0300

    feat: esling config

--- back-end/tests/factories/createRecommendationsFactory.ts
+++ back-end/tests/factories/createRecommendationsFactory.ts
@@ -1,35 +1,34 @@
-import {prisma} from "../../src/database.js";
-import {faker} from "@faker-js/faker";
-import { Recommendation } from "@prisma/client";
+import { prisma } from "../../src/database.js";
+import { faker } from "@faker-js/faker";
 
-function createRecommendation () {
-    return {
-        name: faker.lorem.words(),
-        youtubeLink: `https://www.youtube.com/${faker.random.alpha()}`
-    }
+function createRecommendation() {
+  return {
+    name: faker.lorem.words(),
+    youtubeLink: `https://www.youtube.com/${faker.random.alpha()}`,
+  };
 }
 
-async function createRecommendationWithScore(score: number){
-    const recommendation = {
-        name: faker.lorem.words(),
-        youtubeLink: `https://www.youtube.com/${faker.random.alpha()}`,
-        score: score
-    }
+async function createRecommendationWithScore(score: number) {
+  const recommendation = {
+    name: faker.lorem.words(),
+    youtubeLink: `https://www.youtube.com/${faker.random.alpha()}`,
+    score,
+  };
 
-    await prisma.recommendation.create({
-        data: recommendation
-    })
+  await prisma.recommendation.create({
+    data: recommendation,
+  });
 
-    const result = await prisma.recommendation.findFirst({
-        where: {
-            name: recommendation.name
-        }
-    })
+  const result = await prisma.recommendation.findFirst({
+    where: {
+      name: recommendation.name,
+    },
+  });
 
-    return result
+  return result;
 }
 
-export const createRecommendationFactory =  {
-    createRecommendation,
-    createRecommendationWithScore
-}
+export const createRecommendationFactory = {
+  createRecommendation,
+  createRecommendationWithScore,
+};
--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -1,38 +1,40 @@
-import { prisma } from "./../src/database.js"
+/* eslint-disable no-undef */
+import { prisma } from "./../src/database.js";
 import supertest from "supertest";
 import { createRecommendationFactory } from "./factories/createRecommendationsFactory.js";
 import app from "./../src/app.js";
 
 beforeEach(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
 });
 
 describe("Recommendations tests", () => {
+  it("Create Recommendation Success - name, youtubeLink correct insert exxpect 201", async () => {
+    const recommendation = createRecommendationFactory.createRecommendation();
+    const result = await supertest(app)
+      .post("/recommendations")
+      .send(recommendation);
+    expect(result.status).toBe(201);
+    const isRecommendationRegistered = await prisma.recommendation.findFirst({
+      where: { name: recommendation.name },
+    });
+    expect(isRecommendationRegistered).not.toBeNull();
+  });
 
-    it("Create Recommendation Success - name, youtubeLink correct insert exxpect 201", async () => {
-        const recommendation = createRecommendationFactory.createRecommendation();
-        const result = await supertest(app).post('/recommendations').send(recommendation);
-        expect(result.status).toBe(201);
-        const isRecommendationRegistered = await prisma.recommendation.findFirst({
-            where: { name: recommendation.name },
-        });
-        expect(isRecommendationRegistered).not.toBeNull();
-    })
+  it("Create Recommendation Fail - body not send expect 422", async () => {
+    const result = await supertest(app).post("/recommendations").send({});
+    expect(result.status).toBe(422);
+  });
 
-    it("Create Recommendation Fail - body not send expect 422", async () => {
-        const result = await supertest(app).post('/recommendations').send({});
-        expect(result.status).toBe(422);
-    })
-
-    it("Add a pontuation on Recommendation Succes - expect 200", async () => {
-        const recommendation = await createRecommendationFactory.createRecommendationWithScore(1);
-        const id = recommendation.id;
-        const result = await supertest(app).post(`/recommendations/${id}/upvote`);
-        expect(result.status).toBe(200);
-    })
-
-})
+  it("Add a pontuation on Recommendation Succes - expect 200", async () => {
+    const recommendation =
+      await createRecommendationFactory.createRecommendationWithScore(1);
+    const id = recommendation.id;
+    const result = await supertest(app).post(`/recommendations/${id}/upvote`);
+    expect(result.status).toBe(200);
+  });
+});
 
 afterAll(async () => {
-    await prisma.$disconnect();
+  await prisma.$disconnect();
 });
