05d7cee7e4c0a6b879361a26ce8edae84fa81b50
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-29 12:37:14 -0300

    feat: home e2e tests - score and incorrect link

--- front-end/cypress/e2e/integration/home.cy.js
+++ front-end/cypress/e2e/integration/home.cy.js
@@ -21,4 +21,47 @@ describe("insert recommendation", () => {
     cy.get("button").click();
     cy.wait("@insertRecommendation");
   });
+  it("should show a popup alert in case of link already registered", () => {
+    const recommendation = {
+      name: "Gilsons, Mariana Volker - Devagarinho",
+      youtubeLink: "https://www.youtube.com/watch?v=NPl2N9eQOn4",
+    };
+    cy.visit("http://localhost:3000/");
+    cy.get('input[placeholder="Name"]').type(recommendation.name);
+    cy.get('input[placeholder="https://youtu.be/..."]').type(
+      recommendation.youtubeLink
+    );
+    cy.intercept("POST", "http://localhost:5000/recommendations").as(
+      "insertRecommendation"
+    );
+    cy.get("button").click();
+    cy.wait("@insertRecommendation");
+    cy.on("window:alert", (text) => {
+      expect(text).to.contains("Error creating recommendation!");
+    });
+  });
+  it("should increase recommendation score", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get(`article:first`).within(() => {
+      cy.get("div:last").should("have.text", "0");
+    });
+    cy.get(`article:first`).within(() => {
+      cy.get("svg:first").click();
+    });
+    cy.get(`article:first`).within(() => {
+      cy.get("div:last").should("have.text", "1");
+    });
+  });
+  it("should decrease recommendation score", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get(`article:first`).within(() => {
+      cy.get("div:last").should("have.text", "0");
+    });
+    cy.get(`article:first`).within(() => {
+      cy.get("svg:last").click();
+    });
+    cy.get(`article:first`).within(() => {
+      cy.get("div:last").should("have.text", "-1");
+    });
+  });
 });
