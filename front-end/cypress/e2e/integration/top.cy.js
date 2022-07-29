c38bfd97b9767658c70638ef4203c4cb17e4b666
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-07-29 13:12:51 -0300

    feat: top page e2e tests

--- front-end/cypress/e2e/integration/top.cy.js
+++ front-end/cypress/e2e/integration/top.cy.js
@@ -8,13 +8,15 @@ describe("top page tests e2e", () => {
   });
   it("should return top recommendations", () => {
     const recommendations = createData();
+    const amount = 3;
     cy.visit(`http://localhost:3000/`);
-    cy.contains("Top").click();
-    cy.url().should("equal", `http://localhost:3000/top`);
     cy.intercept("GET", "http://localhost:5000/recommendations/top/10").as(
       "getTopRecommendations"
     );
+    cy.contains("Top").click();
+    cy.url().should("equal", `http://localhost:3000/top`);
     cy.wait("@getTopRecommendations");
     cy.contains(recommendations[0].name);
+    cy.get("article").should("have.length", amount);
   });
 });
